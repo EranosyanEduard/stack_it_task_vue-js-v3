ef92e549e91faa23a35b7502df09933f452bf0f4
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2022-11-14 17:57:48 +0300

    feat: создать index.ts в каталоге src/components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import IButton from './button'
+import IForm, { TForm } from './form'
+import IInput, { TInput, UInput } from './input'
+import IPopUp from './popup'
+import ISpinner from './spinner'
+
+export { IButton, IForm, IInput, IPopUp, ISpinner, type TForm, type TInput, UInput }
