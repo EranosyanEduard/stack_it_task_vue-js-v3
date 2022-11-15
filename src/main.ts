a36f9cb284e1bd59b80d47d59202d582bbd7b392
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2022-11-15 13:32:30 +0300

    feat: сделать компоненты глобальными

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
+import { IButton, IForm, IInput, IPopUp, ISpinner, ITable } from './components'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+/**
+ * @description
+ * Зарегистрировать компоненты в кач-ве глобальных.
+ */
+const defineComps = (): void => {
+    const components = {
+        IButton,
+        IForm,
+        IInput,
+        IPopUp,
+        ISpinner,
+        ITable
+    }
+
+    Object.entries(components).forEach(([name, Comp]) => app.component(name, Comp))
+}
+
+defineComps()
+app.mount('#app')
